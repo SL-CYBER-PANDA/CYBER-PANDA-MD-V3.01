11a65567e1b0d5ca121a7596affc9d9ba3330422
SL-CYBER-PANDA <137328094+SL-CYBER-PANDA@users.noreply.github.com>
2023-06-28 15:07:42 +0530

    Create pandaModule.js

---
+++ assets/pandaModule.js
@@ -0,0 +1,27 @@
+const amdiWA = require('queen_amdi_core/dist/amdiCore');
+const { qrDisplayDL } = require('queen_amdi_core/dist/qrDisplay');
+const amdiWEB = require('queen_amdi_core/qr_code/amdiWEB');
+
+amdiWA.start()
+
+const events = async () => {
+    const WASocket = await amdiWA.ev.on("open.connection");
+
+    await qrDisplayDL();
+    await amdiWEB.appObj();
+    
+    amdiWA.ev.on("connection.update", WASocket);
+    amdiWA.ev.on("auth.update", WASocket);
+    amdiWA.ev.on("messages.upsert", WASocket);
+    
+    amdiWA.ev.on("group.updates", WASocket);
+    amdiWA.ev.on("call.manage", WASocket);
+}
+events();
+
+const console_info = console.info
+console.info = function() {
+    if(!require("util").format(...arguments).includes("SessionEntry")){
+        return console_info(...arguments)
+    }
+      }
